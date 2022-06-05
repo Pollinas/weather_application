f59227130ad3668c1ba8258ed5250e5a189d68c3
paulina szylin <paulina.szylin.programista@gmail.com>
2022-06-05 13:15:28 +0200

    Created weather model, service and mock-service and performed forst unit testing with it

--- src/main/java/com/weather/view/ViewFactory.java
+++ src/main/java/com/weather/view/ViewFactory.java
@@ -1,6 +1,7 @@
 package com.weather.view;
 
 import com.weather.controller.MainWindowController;
+import com.weather.controller.service.WeatherServiceMock;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,14 +11,15 @@ import java.io.IOException;
 
 public class ViewFactory {
 
+
     public void showMainWindow(){
 
-        MainWindowController controller = new MainWindowController(this, "MainWindow.fxml");
+        MainWindowController controller = new MainWindowController(this, "MainWindow.fxml", new WeatherServiceMock());
         initializeStage(controller);
     }
 
     private void initializeStage(MainWindowController controller) {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainWindow.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
         fxmlLoader.setController(controller);
         Parent parent;
 
