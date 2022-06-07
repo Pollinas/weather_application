6b68868c7bc3274d32344945207c300f0a1cea23
paulina szylin <paulina.szylin.programista@gmail.com>
2022-06-07 14:12:25 +0200

    Further development of the parseWeatherDTO method in weatherServiceImpl class

--- src/main/java/com/weather/controller/service/WeatherServiceImpl.java
+++ src/main/java/com/weather/controller/service/WeatherServiceImpl.java
@@ -6,6 +6,7 @@ import com.weather.model.dto.WeatherDTO;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class WeatherServiceImpl implements WeatherService {
         RequestService requestWeatherService = new RequestWeatherMockService();
         WeatherDTO weatherDTO = requestWeatherService.JSONToModelConversion();
 
-        //konwersja Weather DTO na 5 obiektów Weather; w List
+        //konwersja Weather DTO na listę 5 obiektów Weather;
         List<Weather> weathers = parseWeatherDTO(weatherDTO);
 
         //przesłanie tych obiektów do kontrolera MainWindowController
@@ -34,6 +35,15 @@ public class WeatherServiceImpl implements WeatherService {
         Date currentDate = new Date();
         SimpleDateFormat dateFormat= new SimpleDateFormat("dd/MMM/yyyy");
         String currentDateOnly = dateFormat.format(currentDate);
+
+        Date dayTwo = new Date(currentDate.getTime() + (1000 * 60 * 60 * 24));
+        String dayTwoDateOnly = dateFormat.format(dayTwo);
+        Date dayThree = new Date(dayTwo.getTime() + (1000 * 60 * 60 * 24));
+        String dayThreeDateOnly = dateFormat.format(dayThree);
+        Date dayFour = new Date(dayThree.getTime() + (1000 * 60 * 60 * 24));
+        String dayFourDateOnly = dateFormat.format(dayFour);
+        Date dayFive = new Date(dayFour.getTime() + (1000 * 60 * 60 * 24));
+        String dayFiveDateOnly = dateFormat.format(dayFive);
 
         Weather weatherDayOne = new Weather();
         Weather weatherDayTwo = new Weather();
@@ -52,8 +62,12 @@ public class WeatherServiceImpl implements WeatherService {
                     Date.from(Instant.ofEpochSecond(weatherDTO.getList().get(i).getDt()));
             String dateOnly = dateFormat.format(date);
 
-            int hours = date.getHours();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
 
+            int hours = calendar.get(Calendar.HOUR_OF_DAY);
+
+            //check for day one
             if (currentDateOnly.equals(dateOnly) && hours == 12) {
                 weatherDayOne.setDescription(weatherDTO.getList().get(i).getWeatherDescription().get(0).getDescription());
                 weatherDayOne.setDayTemperature(weatherDTO.getList().get(i).getMain().getTemp());
@@ -63,6 +77,15 @@ public class WeatherServiceImpl implements WeatherService {
 
                 weatherDayOne.setNightTemperature(weatherDTO.getList().get(i).getMain().getTemp());
             }
+
+            //check for day two
+
+            //check for day three
+
+            //check for day four
+
+            //check for day five
+
 
 
         }
