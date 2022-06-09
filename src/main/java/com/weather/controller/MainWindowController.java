39d0ebde22ca05ea18c9409e3ff82ea701e8c05b
paulina szylin <paulina.szylin.programista@gmail.com>
2022-06-09 14:28:26 +0200

    Started the process of showing the data in the main view

--- src/main/java/com/weather/controller/MainWindowController.java
+++ src/main/java/com/weather/controller/MainWindowController.java
@@ -4,7 +4,10 @@ import com.weather.controller.service.WeatherService;
 import com.weather.model.Weather;
 import com.weather.view.ViewFactory;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 
 public class MainWindowController {
@@ -22,25 +25,29 @@ public class MainWindowController {
         return "/fxml/" + fxmlName;
     }
 
-
     @FXML
     private TextField firstCityName;
 
+    @FXML
+    private TextField secondCityName;
 
+    @FXML
+    private VBox fistCityItemHolder;
+
+    @FXML
+    private VBox secondCityItemHolder;
 
     @FXML
     void firstCityButtonAction() {
 
+        Node[] nodes = new Node[4];
+        for (int i = 0; i < nodes.length; i++) {
 
-        String cityName = firstCityName.getText();
-
-
+        }
     }
 
     @FXML
     void secondCityButtonAction() {
-
-
 
     }
 
